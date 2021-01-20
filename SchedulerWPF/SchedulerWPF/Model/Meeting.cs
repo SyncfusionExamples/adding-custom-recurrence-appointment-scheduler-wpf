e9309c77a9911ec0b16e5f5f893783baaa180732
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-20 17:51:49 +0530

    WPF-56496 - custom recurrence appointment sample added.

---
+++ SchedulerWPF/SchedulerWPF/Model/Meeting.cs
@@ -0,0 +1,24 @@
+﻿using Syncfusion.Windows.Shared;
+using System;
+using System.Windows.Media;
+
+namespace WpfScheduler
+{
+    public class Meeting
+    {
+        public string EventName { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        public Brush BackgroundColor { get; set; }
+        public Brush ForegroundColor { get; set; }
+        public string RecurrenceRule { get; set; }
+    }
+}
+
+
+
+
+
+
+
+
